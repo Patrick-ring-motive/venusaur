cd8ccaab39061851c13c58a61089e7ce7a28d3f5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 17:39:28 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,13 @@
+const elements = [...document.getElementsByTagName('*')];
+for(const el of elements){
+  if(String(el.getAttribute('class')).includes('GoogleCreativeContainerClass') || /frame/i.test(el.tagName)){
+    el.remove();
+  }
+}
 
+setInterval(()=>{
+  const elements = [...document.querySelectorAll('.GoogleCreativeContainerClass,iframe,frame')];
+  for(const el of elements){
+    el.remove();
+  }
+},300);
