47b6498fa7f0d2d4f93215a819f837b529a380ea
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 19:38:03 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,3 +1,83 @@
+
+  // Wrap in IIFE to create non polluting closures
+  (() => {
+    // fallback stringifier
+    const stringify = x => {
+      try {
+        return JSON.stringify(x);
+      } catch {
+        return String(x);
+      }
+    };
+    // blocks is a list of strings we intend to filter out
+    const blocks = ["adthrive"];
+    // Create a closure map to store instance properties that are in accessible to external viewers
+    // use WeakMap if available for better memory management but regular map also works
+    const $Map = self?.WeakMap ?? Map;
+    // storing the input arguments to the open method so we can access them later in the send method
+    const _openArgs = new $Map();
+    // List of objects in the xhr api, xhr event target is the parent class so we want to patch it last
+    for (const xhr of [XMLHttpRequest, XMLHttpRequestUpload, XMLHttpRequestEventTarget]) {
+      try {
+        // extra IIFE layer for additional closures
+        (() => {
+          // store the original open method
+          const _open = xhr.prototype.open;
+          if (!_open) return;
+          // set up inheritance between new method to old one to maintain other customizations from others
+          xhr.prototype.open = Object.setPrototypeOf(function open(...args) {
+            // store input args in closure map
+            _openArgs.set(this, args);
+            return _open.apply(this, args);
+          }, _open);
+        })();
+
+        (() => {
+          // store the original send method
+          const _send = xhr.prototype.send;
+          if (!_send) return;
+          // set up inheritance between new method to old one to maintain other customizations from others
+          xhr.prototype.send = Object.setPrototypeOf(function send(...args) {
+            // store input args in closure map
+            const openArgs = _openArgs.get(this) ?? [];
+            for (const arg of openArgs) {
+              const sarg = stringify(arg);
+              for (const block of blocks) {
+                if (sarg.includes(block)) return;
+              }
+            }
+            return _send.apply(this, args);
+          }, _send);
+        })();
+
+        // patching a property is similar to patching a method but only for the property getter
+        // this example block the response if it contains one of our string representations
+        for (const res of ['response', 'responseText', 'responseURL', 'responseXML']) {
+          (() => {
+            const _response = Object.getOwnPropertyDescriptor(xhr.prototype, res)?.get;
+            if (!_response) return;
+            Object.defineProperty(xhr.prototype, res, {
+              configurable: true,
+              enumerable: true,
+              get: Object.setPrototypeOf(function response() {
+                for (const block of blocks) {
+                  // block request if it matches list
+                  if (stringify(x).includes(block)) {
+                    console.warn('blocking xhr response', stringify(x));
+                    // return the expected object type but empty
+                    return Object.create(_response.call(this)?.__proto__);
+                  }
+                }
+                return _response.call(this);
+              }, _response)
+            });
+          })()
+        }
+      } catch {}
+    }
+  })();
+
+
 (()=>{
     const $fetch = globalThis.fetch;
     globalThis.fetch = Object.setPrototypeOf(async function fetch(...args){
