134be4ee23b5cbbf5c624e20600a84d68a988c7e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 20:10:42 -0500

    Update web.js

--- web.js
+++ web.js
@@ -109,7 +109,7 @@ for(const el of elements){
 }
 
 setInterval(()=>{
-  const elements = [...document.querySelectorAll('div[class*="adthrive"],[class*="adthrive-ad"],.GoogleCreativeContainerClass,iframe,frame,[src*="ads.adthrive"],.google-ad-manager-fallback-container')];
+  const elements = [...document.querySelectorAll('div[class*="adthrive"],[class*="adthrive-ad"],.GoogleCreativeContainerClass,iframe,frame,object,embed,[src*="ads.adthrive"],.google-ad-manager-fallback-container')];
   for(const el of elements){
     el?..remove?.();
   }
