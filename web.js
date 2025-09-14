07197503efd75b2d915728e97e7d04e6eee2439e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-13 19:26:49 -0500

    Update web.js

--- web.js
+++ web.js
@@ -16,21 +16,21 @@
     },$fetch);
 })();
 
-const elements = [...document.getElementsByTagName('*')];
+const elements = [...document.getElementsByTagName('*'),...document.firstElementChild.children];
 for(const el of elements){
-  const classes = String(el.getAttribute('class'));
+  const classes = String(el?.getAttribute?.('class'));
   if(classes.includes('GoogleCreativeContainerClass')
      || classes.includes('adthrive-ad')
      || classes.includes('google-ad')
-     || /frame|iframe/i.test(el.tagName)
-     || String(el.src).includes('ads.adthrive')){
-    el.remove();
+     || /frame|iframe/i.test(el?.tagName)
+     || String(el?.src).includes('ads.adthrive')){
+    el?.remove?.();
   }
 }
 
 setInterval(()=>{
   const elements = [...document.querySelectorAll('div[class*="adthrive"],[class*="adthrive-ad"],.GoogleCreativeContainerClass,iframe,frame,[src*="ads.adthrive"],.google-ad-manager-fallback-container')];
   for(const el of elements){
-    el.remove();
+    el?..remove?.();
   }
 },300);
