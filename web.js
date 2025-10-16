682532ab6a8d9c06056baf8b2e0f79beb3ba1f60
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-16 13:41:58 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,6 +1,88 @@
 (() => {
     if (globalThis['&venusaur']) return;
     globalThis['&venusaur'] = true;
+
+
+    ////////////////
+    ////linkeding antifreeze
+    ///////////////
+
+    (() => {
+    const WeakRefMap = (() => {
+        const _weakRefMap = new Map();
+        return class WeakRefMap extends Map {
+            get(key) {
+                const ref = _weakRefMap.get(key);
+                const value = ref?.deref?.();
+                if (value === undefined) {
+                    _weakRefMap.delete(key);
+                }
+                return value;
+            }
+            set(key, value) {
+                _weakRefMap.set(key, new WeakRef(value));
+                return this;
+            }
+            delete(key) {
+                return _weakRefMap.delete(key);
+            }
+            has(key) {
+                const value = _weakRefMap.get(key)?.deref?.();
+                if (value === undefined) {
+                    _weakRefMap.delete(key);
+                    return false;
+                }
+                return true;
+            }
+        }
+    })();
+    let logged = false;
+    let last;
+    const fetchCache = new WeakRefMap();
+    const _fetch = self.fetch;
+    self.fetch = Object.setPrototypeOf(async function fetch(...args) {
+        const url = String(args[0]?.url ?? args[0]);
+        try {
+            const fromCache = fetchCache.get(url);
+            if (fromCache) {
+                if(url != last){
+                    console.log('From cache', url);
+                    last = url;
+                }
+                const res = await fromCache;
+                if (!/^[12]\d\d$/.test(response?.status)) {
+                    fetchCache.delete(url);
+                } else {
+                    return res.clone();
+                }
+            }
+            if (url.includes('chrome-extension://')) {
+                if (!logged) {
+                    console.warn('Blocking fetch of chrome-extension:// urls');
+                    logged = true;
+                }
+                return new Response('{}', { status: 200, statusText: 'OK' });
+            }
+            const presponse = _fetch.apply(this, args);
+            fetchCache.set(url, presponse);
+            const response = (await presponse);
+            if (!/^[12]\d\d$/.test(response?.status)) {
+                fetchCache.delete(url);
+            }
+            return response.clone();
+        } catch (e) {
+            fetchCache.delete(url);
+            return new Response(e?.stack, {
+                status: 500,
+                statusText: e?.message
+            });
+        }
+    }, _fetch);
+})();
+
+    ///////////////////////
+    ///
+    /////////////////////
     const pageLog = document.createElement('log');
     document.firstElementChild.appendChild(pageLog);
     self.log = (...x) => {
